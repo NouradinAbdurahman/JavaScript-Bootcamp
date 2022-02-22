20f2bdbf3ce30b7278d7e12b77992a443ff73f28
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-02-22 22:23:14 +0300

    Variables intro

---
+++ 02 Variables intro.js
@@ -0,0 +1,22 @@
+/*
+    Variables intro
+    - Syntax( Keyword | Variable Name | Assignment Operator | Variable Value)
+    - Variable without Var 
+    - Multiple Variables in the same line
+    - Loosly typed VS Strongly typed
+*/
+
+// Syntax( Keyword | Variable Name | Assignment Operator | Variable Value)
+var name = "Nouradin";
+console.log(`Hello, ${name}`);
+
+// Variable without Var
+user = "Azzuz";
+console.log(`Hello, ${user}`);
+
+// Multiple Variables in the same line
+var fname = "Nouradin",
+    lname = "Aden",
+    age = 21;
+
+console.log(`Hello, ${fname} ${lname} Your age is: ${age}`);
