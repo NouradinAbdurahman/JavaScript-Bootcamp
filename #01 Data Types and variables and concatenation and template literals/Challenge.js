087061d77b1a87b4173b8b039b6e17c8d37b0c03
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-02-24 15:09:02 +0300

    First  Section Finished.

--- #01 Data Types and variables and concatenation and template literals/Challenge.js
+++ #01 Data Types and variables and concatenation and template literals/Challenge.js
@@ -10,5 +10,6 @@ let div = `
         </div>
     </div>
 `;
+
 //The chalange was to create this div four times but we can use the repeat method in javascript so we don't have to write that div four times.
 // document.write(div.repeat(4));
