0bd400d38427c67be9763c4abb6bbb0973bf5d5c
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-02-24 15:08:06 +0300

    Second Section Finished.

---
+++ #02  Arithmetic Operators and Unary Plus and Negation Operators and Type Coercion and Assignment Operators/04 Assignment Operators.js
@@ -0,0 +1,21 @@
+/*
+    Assignment Operators
+*/
+let x = 10;
+x = x + 5; // Old way and normal waye 
+x = x - 1; // Old way and normal waye 
+x = x * 10; // Old way and normal waye 
+x = x / 2; // Old way and normal waye 
+x = x % 3; // Old way and normal waye 
+x = x ** 4; // Old way and normal waye 
+console.log(`Assignment Operators old way: ${x}`);
+
+// There is advanced way to do that 
+let y = 100;
+y += 100; // New way and better way
+y -= 1; // New way and better way
+y *= 10; // New way and better way
+y /= 2; // New way and better way
+y %= 1; // New way and better way
+y *= 4; // New way and better way
+console.log(`Assignment Operators New way and better way: ${y}`);
---
+++ #02  Arithmetic Operators and Unary Plus and Negation Operators and Type Coercion and Assignment Operators/Challenge.js
@@ -0,0 +1,8 @@
+// Operators Challenges
+console.log("Operators Challenges");
+let a = "-100";
+let b = "20";
+let c = 30;
+let d = true;
+console.log(-a * +b + c + +d - c - +d); // The result must be 2000
+console.log(-a + +b + c + +d + +b + +d + +d); // The result must be 173
