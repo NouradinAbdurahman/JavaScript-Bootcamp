1820d52018f08ff32b32d54d51b2e9cc8a6cf1e8
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-02-25 22:23:43 +0300

    Added String method

---
+++ #04 String/01 String Methods Part 1.js
@@ -0,0 +1,37 @@
+/*
+    String Method
+    - Access with index[]
+    - Access with chartAt() // character at position
+    - length
+    - trim() // it will remove the spaces 
+    - toUpperCase() // it will return all the string to upper case
+    - toLoweCase() //  it will return all the string to lower case
+    - chain methods // this method means you can write all the methods in the same time one by one like = x.trim().chartAt(0)......etc.
+*/
+
+let theName = "Nouradin";
+let theList = [1,2,3,4,5,6,7,8,9,10];
+
+// Access with index[]
+console.log(`The index is: ${theName[0]}`);
+
+// Access with cartAt()
+console.log(`The chartAt is: ${theName.charAt(7)}`);
+
+// length
+console.log(`The length is: ${theName.length}`);
+
+
+let theTrim = "   Nouradin    Aden";
+console.log(`This is without the trim method: ${theTrim}`);
+
+// trim
+console.log(`This is with the trim method: ${theTrim.trim()}`);
+
+
+// toUpperCase()
+console.log(`The UpperCase of thName Variables is: ${theName.toUpperCase()}`);
+
+
+// toLowerCase()
+console.log(`The LowerCase of thName Variables is: ${theName.toLowerCase()}`);
