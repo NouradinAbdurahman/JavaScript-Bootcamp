01884da52815e8ed4865ef013ffca354bca31f5a
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-03-06 09:28:35 +0300

    Array

--- #08 Array/Array Challenge.js
+++ #08 Array/Array Challenge.js
@@ -2,18 +2,23 @@
   Array Challenge
 */
 
-let zero = 0;
-
-let counter = 3;
-
-let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+let myFriends = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
 // Write Code Here
 
-console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
+console.log(myFriends.slice(0, myFriends.length - 2).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
 
-console.log(my.slice("????")); // ["Elham", "Mazero"]
 
-console.log(); // "Elzero"
+console.log(myFriends.slice(1,3).reverse()); // ["Elham", "Mazero"]
 
-console.log(); // "rO"
+
+myFriends[myFriends.length] = "Elzero";
+myFriends.splice(0 , 6); // "Elzero"
+console.log(myFriends)
+
+let x = myFriends[0][4];
+let y = myFriends[0][5];
+let z = x.concat(y.toUpperCase());
+console.log(z); // "rO"
+
+
